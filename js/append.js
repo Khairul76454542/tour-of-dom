04fe5866514a8f2cb98bb87ff6c83042f072c8a3
khairul76454542 <khairul76454542@gmail.com>
2023-08-14 07:05:59 +0600

    first commit

---
+++ js/append.js
@@ -0,0 +1,46 @@
+console.log("append js");
+
+// 1. where to add
+const placesList = document.getElementById("places-list");
+// 2. what to be added
+const li = document.createElement("li");
+li.innerText = "pahartoli bon";
+
+// 3. add the child
+placesList.appendChild("li");
+
+// 1. where to ad
+const mainContainer = Document.getElementById("main-container");
+
+// 2. what to be added
+const section = Document.createElement("section");
+const h1 = document.createElement("h1");
+h1.innerText = "my food list";
+section.appendChild(h1);
+
+const ul = Document.createElement("ul");
+const li1 = document.createElement(li);
+li1.innerText = "biryani";
+ul.appendChild(li1);
+const li2 = Document.createElement("li");
+li2.innerText = "borhani";
+ul.appendChild(li2);
+const li3 = Document.createElement("li");
+li3.innerText = "salad";
+ul.appendChild(li3);
+
+section.appendChild(ul);
+
+mainContainer.appendChild(section);
+
+// set innerHTML directly
+const sectionDress = Document.createElement("section");
+sectionDress.innerHTML = `
+<h1> My Dress section </h1>
+<ul>
+    <li>T-shirt</li>
+    <li>Lungi</li>
+    <li>sando genji</li>
+</ul>    
+`;
+mainContainer.appendChild(sectionDress);
---
+++ js/styleIn.js
@@ -0,0 +1,9 @@
+const sections = document.querySelectorAll("section");
+for (const section of sections) {
+  // console.log(section);
+  section.style.border = "2px solid steelblue";
+  section.style.marginBottom = "5px";
+  section.style.borderRadius = "15px";
+  section.style.paddingLeft = "7px";
+  section.style.backgroundColor = "lightgray";
+}
